edcc64d2edd2a3bbe9c9fc21c411125ba33d910b
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2012-04-30 12:05:22 +0800

    Update the description and license.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,20 @@
-/* vim:set et sts=4: */
+/* -*- coding: utf-8; indent-tabs-mode: nil; tab-width: 4; c-basic-offset: 4; -*- */
+/**
+ * Copyright (C) 2012 Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <ibus.h>
 #include "engine.h"
@@ -71,7 +87,7 @@ int main(int argc, char **argv)
     GOptionContext *context;
 
     /* Parse the command line */
-    context = g_option_context_new ("- a real traditional zhuyin Chinese input method");
+    context = g_option_context_new ("- a zhuyin (phonetic) Chinese input method");
     g_option_context_add_main_entries (context, entries, "ibus-zhuyin");
 
     if (!g_option_context_parse (context, &argc, &argv, &error)) {
@@ -84,3 +100,5 @@ int main(int argc, char **argv)
     init ();
     ibus_main ();
 }
+
+/* vim:set fileencodings=utf-8 tabstop=4 expandtab shiftwidth=4 softtabstop=4: */
