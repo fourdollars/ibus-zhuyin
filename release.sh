59d19fbfdda15809ccad4ab587d72a4b5fbbca90
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2013-03-15 23:44:21 +0800

    Add a release script for rpm.

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# Convert git log to GNU-style ChangeLog file.
+
+git log --date-order --date=short --since="Sat Apr 21 13:05:11 2012 +0800" | \
+  sed -e '/^commit.*$/d' | \
+  awk '/^Author/ {sub(/\\$/,""); getline t; print $0 t; next}; 1' | \
+  sed -e 's/^Author: //g' | \
+  sed -e 's/>Date:   \([0-9]*-[0-9]*-[0-9]*\)/>\t\1/g' | \
+  sed -e 's/^\(.*\) \(<.*>\)\t\(.*\)/\3  \1 \2/g' -e 's/^    /\t/g' > ChangeLog
+
+make distcheck
+make rpm
