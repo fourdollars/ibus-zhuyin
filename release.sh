6676374321acaf547ae97055cbf3a12739c4c6d1
Shih-Yuan Lee (FourDollars) <fourdollars@gmail.com>
2013-03-16 18:15:41 +0800

    Add srpm rule.

--- release.sh
+++ release.sh
@@ -9,4 +9,4 @@ git log --date-order --date=short --since="Sat Apr 21 13:05:11 2012 +0800" | \
   sed -e 's/^\(.*\) \(<.*>\)\t\(.*\)/\3  \1 \2/g' -e 's/^    /\t/g' > ChangeLog
 
 make distcheck
-make rpm
+make srpm
